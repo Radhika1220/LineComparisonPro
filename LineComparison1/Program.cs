9e902334383509f8ca12d3977f9cc1fd09b1e50c
Radhika <radhika.shankar1220@gmail.com>
2021-07-01 15:10:43 +0530

    [Radhika] Adding a oops concepts

--- LineComparison1/Program.cs
+++ LineComparison1/Program.cs
@@ -4,32 +4,39 @@ namespace LineComparison1
 {
     class Program
     {
-        static void Main(string[] args)
+        public int len1;
+        public int len2;
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
+     
+
+        public Program()
         {
             double[] StoringLength = new double[5];
-            for (int i = 1; i <= 2; i++)
+            for (int i = 1; i < 2; i++)
             {
                 Console.WriteLine("Enter value for  coordinate x1");
-                int x1 = Convert.ToInt32(Console.ReadLine());
+                this.x1 = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Enter value for   coordinate y1");
-                int y1 = Convert.ToInt32(Console.ReadLine());
+                this.y1 = Convert.ToInt32(Console.ReadLine());
 
 
                 Console.WriteLine("Enter value for  first coordinate x2");
-                int x2 = Convert.ToInt32(Console.ReadLine());
+                this.x2 = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Enter value for  first coordinate y2");
-                int y2 = Convert.ToInt32(Console.ReadLine());
-
-
-                double length1 = Program.FindingLength(x1, x2, y1, y2);
-                Console.WriteLine("Line Length :" + length1);
-                StoringLength[i] = length1;
+                this.y2 = Convert.ToInt32(Console.ReadLine());
             }
-            Program obj = new Program();
-            obj.EqualityMethod(StoringLength[1].ToString(), StoringLength[2].ToString());
-            obj.comparingLines(StoringLength[1].ToString(), StoringLength[2].ToString());
+        }
+        public double FindingLength()
+        {
+            //Calculating a line length
+            double LineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double len = Math.Round(LineLength, 2);
+            return len;
 
         }
 
@@ -44,17 +51,10 @@ namespace LineComparison1
                 Console.WriteLine("Lines are not equal");
             }
         }
-        public static double FindingLength(int x1, int x2, int y1, int y2)
-        {
-            //Calculating a line length
-            double LineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            double len = Math.Round(LineLength, 2);
-            return len;
 
-        }
-        public  void comparingLines(string len1,string len2)
+        public void comparingLines(string len1, string len2)
         {
-            if(len1.CompareTo(len2)>0)
+            if (len1.CompareTo(len2) > 0)
             {
                 Console.WriteLine("Line1 is greater than line2");
             }
@@ -67,6 +67,23 @@ namespace LineComparison1
                 Console.WriteLine("both lines are equal");
             }
         }
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program();
+            double length1 = obj1.FindingLength();
+            Program obj2 = new Program();
+            double length2 = obj2.FindingLength();
+
+            Console.WriteLine("Length of First Line " + length1);
+            Console.WriteLine("Length of Second Line " + length2);
+            obj1.EqualityMethod(length1.ToString(), length2.ToString());
+            obj2.comparingLines(length1.ToString(), length2.ToString());
+
+
+
+        }
+
+
     }
 }
 
